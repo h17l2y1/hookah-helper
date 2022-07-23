c59aafb8247991a93aea90c3ef8be663ab6f3301
vladyslav.sharmanov <vladislavvillya@gmail.com>
2022-07-23 18:56:28 +0300

    image2

---
+++ ui/src/app/admin/image-loader/image-loader.component.ts
@@ -0,0 +1,55 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Observable} from "rxjs";
+
+@Component({
+    selector: 'image-loader',
+    templateUrl: './image-loader.component.html',
+    styleUrls: ['./image-loader.component.scss']
+})
+export class ImageLoaderComponent implements OnInit {
+    @Output() getImage = new EventEmitter<File>();
+
+    selectedFile?: File;
+    previews: string[] = [];
+    imageInfos?: Observable<any>;
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+    selectFiles(event: any): void {
+        this.selectedFile = event.target.files[0];
+        this.previews = [];
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+            this.previews.push(e.target.result);
+        };
+        reader.readAsDataURL(this.selectedFile);
+        this.getImage.emit(this.selectedFile);
+
+    }
+
+    upload(idx: number, file: File): void {
+        // this.progressInfos[idx] = {value: 0, fileName: file.name};
+        if (file) {
+            // this.uploadService.upload(file).subscribe(
+            //     (event: any) => {
+            //       if (event.type === HttpEventType.UploadProgress) {
+            //         this.progressInfos[idx].value = Math.round(100 * event.loaded / event.total);
+            //       } else if (event instanceof HttpResponse) {
+            //         const msg = 'Uploaded the file successfully: ' + file.name;
+            //         this.message.push(msg);
+            //         this.imageInfos = this.uploadService.getFiles();
+            //       }
+            //     },
+            //     (err: any) => {
+            //       this.progressInfos[idx].value = 0;
+            //       const msg = 'Could not upload the file: ' + file.name;
+            //       this.message.push(msg);
+            //     });
+        }
+    }
+
+}
